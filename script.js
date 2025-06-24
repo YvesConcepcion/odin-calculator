ebc69b1f17bdd3b6edef78de51e4b5d40a68667d
YvesConcepcion <118972229+YvesConcepcion@users.noreply.github.com>
2025-06-24 19:55:40 +0800

    Fix eslint errors

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+/* eslint-disable quotes */
+/* eslint-disable no-alert */
+/* eslint-disable no-unused-vars */
 // create variables num1 + operator + num2 + result
+// basic math operators
 function add(a, b) {
   return a + b;
 }
@@ -11,3 +15,17 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+// variables
+let num1;
+let num2;
+let operator;
+
+// let function = operate() {//
+const digit = document.getElementById("digit");
+// create function to populate display and store variable
+function displayDigit() {
+  // document.getElementById("digit").innerText = "test";
+  // console.log("test");
+  alert("test");
+}
+digit.addEventListener("click", displayDigit);
