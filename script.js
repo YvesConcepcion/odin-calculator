0d3a63e6cf6636b51a274a37b676744747baceb4
YvesConcepcion <118972229+YvesConcepcion@users.noreply.github.com>
2025-07-06 14:30:27 +0800

    Add variable to populateDisplay for operate function

--- script.js
+++ script.js
@@ -5,38 +5,42 @@
 // create variables num1 + operator + num2 + result
 // basic math operators
 
-function operate(num1, num2, operate) {
-  function add(a, b) {
-    return a + b;
-  }
-  function subtract(a, b) {
-    return a - b;
-  }
-  function multiply(a, b) {
-    return a * b;
-  }
-  function divide(a, b) {
-    return a / b;
-  }
+let num1;
+let num2;
+let operator;
+
+function add(a, b) {
+  console.log(a + b);
+  return a + b;
 }
-// let function = operate() {//
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+function operate(operator, a, b) {
+  // Make the calculator work! You’ll need to store the first and second numbers input by the user and then operate() on them when the user presses the = button, according to the operator that was selected between the numbers.
+}
+
 let digits = document.querySelectorAll(".digit");
 let display = document.getElementById("display");
-let add = document.getElementById("add");
 
 digits.forEach(function (e) {
   e.addEventListener("click", populateDisplay);
 });
 
-//if operator is pressed + = -
-
-// add.addEventListener("click",function(){
-
-// });
-
 function populateDisplay() {
-  if (display.textContent === "0") display.textContent = this.textContent;
-  else {
+  let num;
+  if (display.textContent === "0") {
+    display.textContent = this.textContent;
+    num = Number(display.textContent);
+  } else {
     display.textContent += this.textContent;
+    num = Number(display.textContent);
   }
+  return num;
 }
