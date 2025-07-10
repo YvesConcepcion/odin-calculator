e3678d75c62e6eed85c4689e55ef2fc3470814cf
YvesConcepcion <118972229+YvesConcepcion@users.noreply.github.com>
2025-07-10 20:31:36 +0800

    Fix operator click behavior and reset flag after input

--- script.js
+++ script.js
@@ -8,9 +8,10 @@
 // main calculator
 function main() {
   let num1 = 0;
-  let num2 = 0;
+  let num2;
   let currentNum = 1;
   let operator;
+  let activeOperator = false;
   let total;
 
   function add(a, b) {
@@ -34,6 +35,13 @@ function main() {
     }
   }
 
+  function switchNum() {
+    if (currentNum === 1) {
+      currentNum = 2;
+    } else {
+      currentNum = 1;
+    }
+  }
   let digits = document.querySelectorAll(".digit");
   let display = document.getElementById("display");
   let operators = document.querySelectorAll(".operator");
@@ -44,14 +52,22 @@ function main() {
 
   operators.forEach(function (e) {
     e.addEventListener("click", function () {
-      operator = e.id;
-      console.log(operator);
+      if (!activeOperator === true) {
+        operator = e.id;
+        console.log(operator);
+        switchNum();
+        console.log(num1, num2, operator);
+        activeOperator = true;
+      }
     });
   });
 
   function operate(num1, operator, num2) {}
   function populateDisplay() {
     let num;
+    if (activeOperator === true) {
+      activeOperator = false;
+    }
     if (display.textContent === "0") {
       display.textContent = this.textContent;
       num = Number(display.textContent);
